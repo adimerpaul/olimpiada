b70bb7c56a3888853a3e8539a7bb1d786602974a
AFLopezG <lopez.gutierrez.a07@gmail.com>
2023-10-03 11:28:23 -0400

    mod

--- back/routes/api.php
+++ back/routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
-Route::post('/student', [\App\Http\Controllers\UserController::class,'store']);
+Route::post('/student', [\App\Http\Controllers\StudentController::class,'store']);
 
 
 Route::post('/login',[\App\Http\Controllers\UserController::class,'login']);
